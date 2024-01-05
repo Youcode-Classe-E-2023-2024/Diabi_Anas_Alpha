b8db1fd922874b022df119cfe0d161c7ff04051d
Anas-Diabi <33428345+Hackermans007@users.noreply.github.com>
2024-01-05 11:19:11 +0100

    login/register

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -4,6 +4,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     $username = $_POST["username"];
     $pwd = $_POST["pwd"];
+    $hashedPassword = password_hash($pwd, PASSWORD_DEFAULT);
+    $pwd = $hashedPassword;
 
     try {
         require_once 'dbh.inc.php';
@@ -23,27 +25,40 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $result = get_user($pdo, $username);
 
         if (is_username_wrong($result)) {
-            $errors["login_incorrect"] = "Incorrect login info";
+            $errors["login_incorrect"] = "Incorrect username";
         }
         if (!is_username_wrong($result) && !is_password_wrong($pwd, $result["pwd"])) {
-            $errors["login_incorrect"] = "Incorrect login info";
+            $errors["login_incorrect"] = "Incorrect password";
         }
 
         require_once 'config_session.inc.php';
 
 
         if ($errors) {
-            $_SESSION["errors_signup"] = $errors;
-            $inputData = [
-                "username" => $username,
-                "email" => $email
-            ];
-            $_SESSION["input_data"] = $inputData;
+            $_SESSION["errors_login"] = $errors;
 
-
+        
             header("location: ../index.php");
             die();
         }
+
+        //Creating a new session id
+
+            $newSessionId = session_create_id();
+            $sessionId = $newSessionId . "_" . $result["id"] ;
+            session_id($sessionId);
+           
+            $_SESSION["user_id"] = $result["id"];
+            $_SESSION["user_username"] = htmlspecialchars($result["username"]);
+
+            $_SESSION["last_regeneration"] = time();
+
+            header("location: ../index.php?login=success");
+            $pdo = null;
+            $statement = null;
+
+            die();
+
     } catch (PDOException $e) {
         die("Query failed: " . $e->getMessage());
     }
--- index.php
+++ index.php
@@ -1,10 +1,12 @@
-<?php  
-    require_once 'includes/config_session.inc.php';
-    require_once 'includes/signup_view.inc.php';
+<?php
+require_once 'includes/config_session.inc.php';
+require_once 'includes/signup_view.inc.php';
+require_once 'includes/login_view.inc.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,62 +16,118 @@
     <!-- style -->
     <link rel="stylesheet" href="style/style.css">
 </head>
+
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <!-- Login Form -->
-            <div class="card">
-                <div class="card-header">
-                    <h2 class="text-center">Login</h2>
-                </div>
-                <div class="card-body">
-                   <form action="includes/login.inc.php" method="POST">
-                        <div class="form-group">
-                            <label for="loginEmail">User Name:</label>
-                            <input type="text" class="form-control" id="loginusername" name="username" >
-                        </div>
-                        <div class="form-group">
-                            <label for="loginPassword">Password:</label>
-                            <input type="password" class="form-control" id="loginPassword" name="pwd" >
-                        </div>
-                        <button type="submit" name="submit" class="btn btn-primary">Login</button>
-                    </form>
-                </div>
-            </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+            </ul>
+            <?php
+            output_username()
+            ?>
+            <form class="form-inline my-2 my-lg-0" action="includes\logout.inc.php" method="post">
+                <button class="btn btn-danger">Logout</button>
+
+            </form>
         </div>
+    </nav>
 
-        <div class="col-md-6">
-            <!-- Signup Form -->
-            <div class="card">
-                <div class="card-header">
-                    <h2 class="text-center">Registration</h2>
-                </div>
-                <div class="card-body">
-                    <form action="includes/signup.inc.php" method="POST">
+    <?php
+
+if (!isset($_SESSION["user_id"])) { ?>
+
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <!-- Login Form -->
+                <div class="card">
+                    <div class="card-header">
+                        <h2 class="text-center">Login</h2>
+                    </div>
+                    <div class="card-body">
+                       
+                        
+                            <form action="includes/login.inc.php" method="POST">
+                            <div class="form-group">
+                                <label for="loginEmail">User Name:</label>
+                                <input type="text" class="form-control" id="loginusername" name="username">
+                            </div>
+                            <div class="form-group">
+                                <label for="loginPassword">Password:</label>
+                                <input type="password" class="form-control" id="loginPassword" name="pwd">
+                            </div>
+                            <button type="submit" name="submit" class="btn btn-primary">Login</button>
+                        </form>
+                       
+                        
+
+
                         <?php
 
-                        input_data();
-                          
+                        check_login_errors();
+
                         ?>
-                        <button type="submit" name="submit" class="btn btn-success">Register</button>
-                    </form>
-                    <?php
-                    
-                    check_signup_errors();
-                    
-                    ?>
+                    </div>
+                </div>
+            </div>
+            <?php } 
+                        ?>
+            <div class="col-md-6">
+                <!-- Signup Form -->
+                <div class="card">
+                    <div class="card-header">
+                        <h2 class="text-center">Registration</h2>
+                    </div>
+                    <div class="card-body">
+                        <form action="includes/signup.inc.php" method="POST">
+                            <?php
+
+                            input_data();
+
+                            ?>
+                            <button type="submit" name="submit" class="btn btn-success">Register</button>
+                        </form>
+                        <?php
+
+                        check_signup_errors();
+
+                        ?>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-<!-- Bootstrap JS and Popper.js -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <!-- Bootstrap JS and Popper.js -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 
 </body>
+
 </html>
